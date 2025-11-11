602ebe5ff15e1c47c94bc1b3fd0db7b6a228cf5b
mello <falmelopros@gmail.com>
2025-11-11 18:55:45 +0300

    signin form and session management

--- apps/front/src/lib/actions/auth.ts
+++ apps/front/src/lib/actions/auth.ts
@@ -4,34 +4,74 @@ import { SignUpFormState } from "../types/formState";
 import { fetchGraphQL } from "../fetchGraphQL";
 import { print } from "graphql";
 import { SignUpFormSchema } from "../zodSchemas/signUpFormSchema";
-import { CREATE_USER_MUTATION } from "../gqlQueries";
-
+import { CREATE_USER_MUTATION, SIGN_IN_MUTATION } from "../gqlQueries";
+import { revalidatePath } from "next/cache";
+import { LoginFormSchema } from "../zodSchemas/loginFormSchema";
 
 export async function signUp(
-    state: SignUpFormState,
-    formData: FormData
-  ): Promise<SignUpFormState> {
-    const validatedFields = SignUpFormSchema.safeParse(
-      Object.fromEntries(formData.entries())
-    );
-  
-    if (!validatedFields.success)
-      return {
-        data: Object.fromEntries(formData.entries()),
-        errors: validatedFields.error.flatten().fieldErrors,
-      };
-  
-    const data = await fetchGraphQL(print(CREATE_USER_MUTATION), {
-      input: {
-        ...validatedFields.data,
-      },
-    });
-  
-    if (data.errors)
-      return {
-        data: Object.fromEntries(formData.entries()),
-        message: "Something went wrong",
-      };
-    redirect("/auth/signin");
+  state: SignUpFormState,
+  formData: FormData
+): Promise<SignUpFormState> {
+  const validatedFields = SignUpFormSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+
+  if (!validatedFields.success)
+    return {
+      data: Object.fromEntries(formData.entries()),
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+
+  const data = await fetchGraphQL(print(CREATE_USER_MUTATION), {
+    input: {
+      ...validatedFields.data,
+    },
+  });
+
+  if (data.errors)
+    return {
+      data: Object.fromEntries(formData.entries()),
+      message: "Something went wrong",
+    };
+  redirect("/auth/signin");
+}
+
+export async function signIn(
+  state: SignUpFormState,
+  formData: FormData
+): Promise<SignUpFormState> {
+  const validatedFields = LoginFormSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+
+  if (!validatedFields.success)
+    return {
+      data: Object.fromEntries(formData.entries()),
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+
+  const data = await fetchGraphQL(print(SIGN_IN_MUTATION), {
+    input: {
+      ...validatedFields.data,
+    },
+  });
+
+  if (data.errors) {
+    return {
+      data: Object.fromEntries(formData.entries()),
+      message: "Invalid Credentials",
+    };
   }
-  
+
+  // Create a session
+  await createSession({
+    user: {
+      id: data.signIn.id,
+      name: data.signIn.name,
+      avatar: data.signIn.avatar,
+    },
+    accessToken: data.signIn.accessToken,
+  });
+  revalidatePath("/");
+  redirect("/");
+}
